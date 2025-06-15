8a8ec438ee862c7c0b79ef3714bd9d3bcb3c95b3
ahronSteinbruch <139044285+ahronSteinbruch@users.noreply.github.com>
2025-06-15 15:03:38 +0300

    add the class Sensor Pulse

---
+++ SensorPulse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investigation
+{
+    internal class SensorPulse : Sensor
+    {
+        int lifeAmount;
+        public SensorPulse(int lifeAmount, SensorType sensoreNum) : base(sensoreNum)
+        {
+            this.lifeAmount = lifeAmount;
+        }
+        public void Activate()
+        {
+            Console.WriteLine($"i am Sensore pulse");
+            lifeAmount--;
+        }
+
+    }
+}
