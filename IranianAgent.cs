cb5db8464e3ba97db03ff41518fbd6f900cf1859
ahronSteinbruch <139044285+ahronSteinbruch@users.noreply.github.com>
2025-06-15 12:36:44 +0300

    add func Exposed to ret all the expose Sensores

--- IranianAgent.cs
+++ IranianAgent.cs
@@ -11,21 +11,25 @@ namespace investigation
         public IranAgentRank rank;
 
         List<Sensor> sensors;
-
-        public List<SensorType> RekSensors;
-
-        Dictionary<int, SensorType> ExposedSencors;
+        public List<SensorType> RekSensors { get; private set; }
+        public Dictionary<SensorType, int> ExposedSencors { get; private set; }
 
 
         public IranianAgent(IranAgentRank rank)
         {
             sensors = new List<Sensor>();
             RekSensors = new List<SensorType>();
+            ExposedSencors = new();
 
             for (int i = 0; i < (int)rank; i++)
             {
                 SensorType randomType = GetRandomSensorType(new Random());
                 RekSensors.Add(randomType);
+
+                if (ExposedSencors.ContainsKey(randomType))
+                    ExposedSencors[randomType]++;
+                else
+                    ExposedSencors[randomType] = 1;
             }
         }
 
@@ -33,6 +37,15 @@ namespace investigation
         {
             var values = Enum.GetValues(typeof(SensorType));
             return (SensorType)values.GetValue(rand.Next(values.Length));
+        }
+
+        public Dictionary<SensorType,int> Exposed(Sensor s)
+        {
+            if (ExposedSencors.ContainsKey(s.Type) && ExposedSencors[s.Type] > 0)
+            {
+                ExposedSencors[s.Type]--;
+            }
+            return this.ExposedSencors;
         }
 
     }
